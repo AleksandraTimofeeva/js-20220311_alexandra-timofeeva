5aac4eb204032f4493a313b765b0d9fde2530e21
atimofeeva <welcomeToApt1>
2022-04-28 13:28:22 +0200

    Create column chart 2

--- 07-async-code-fetch-api-part-1/1-column-chart/index.js
+++ 07-async-code-fetch-api-part-1/1-column-chart/index.js
@@ -3,5 +3,127 @@ import fetchJson from './utils/fetch-json.js';
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
 export default class ColumnChart {
+  element;
+  subElements = {};
+  chartHeight = 50;
 
+  constructor({
+    label = '',
+    link = '',
+    formatHeading = data => data,
+    url = '',
+    range = {
+      from: new Date(),
+      to: new Date(),
+    }
+  } = {}) {
+    this.url = new URL(url, BACKEND_URL);
+
+    this.range = range;
+    this.label = label;
+    this.link = link;
+    this.value = formatHeading;
+
+    this.render();
+    this.update(this.range.from, this.range.to);
+  }
+
+  render() {
+    const element = document.createElement('div');
+    element.innerHTML = this.getTemplate();
+
+    this.element = element.firstElementChild;
+    this.subElements = this.getSubElements(this.element);
+  }
+
+  getTemplate() {
+    return `
+      <div class="column-chart column-chart_loading" style="--chart-height: ${this.chartHeight}">
+        <div class="column-chart__title">
+          Total ${this.label}
+          ${this.link ? `<a href=${this.link} class="column-chart__link">View all</a>` : ''}
+        </div>
+        <div class="column-chart__container">
+          <div data-element="header" class="column-chart__header"></div>
+          <div data-element="body" class="column-chart__chart">
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  async update(from, to) {
+    this.element.classList.add('column-chart_loading');
+
+    const data = await this.loadData(from, to);
+    this.setNewRange(from, to);
+
+    if (data && Object.values(data).length) {
+      this.subElements.header.textContent = this.getHeaderValue(data);
+      this.subElements.body.innerHTML = this.getColumnBody(data);
+
+      this.element.classList.remove('column-chart_loading');
+    }
+
+    this.data = data;
+    return this.data;
+  }
+
+  async loadData(from, to) {
+    this.url.searchParams.set('from', from.toISOString());
+    this.url.searchParams.set('to', to.toISOString())
+    return await fetchJson(this.url);
+  }
+
+  getHeaderValue(data) {
+    return this.formatHeading(Object.values(data).reduce((accum, item) => (accum + item), 0));
+  }
+
+  getColumnBody(data) {
+    const maxValue = Math.max(...Object.values(data));
+
+    return Object.entries(data).map(([key, value]) => {
+      const scale = this.chartHeight / maxValue;
+      const percent = (value / maxValue * 100).toFixed(0);
+      const tooltip = `<span>
+        <small>${key.toLocaleString('default', {dateStyle: 'medium'})}</small>
+        <br>
+        <strong>${percent}%</strong>
+      </span>`;
+
+      return `<div style="--value: ${Math.floor(value * scale)}" data-tooltip="${tooltip}"></div>`;
+    }).join('');
+  }
+
+  getSubElements(element) {
+    const elements = element.querySelectorAll('[data-element]');
+
+    return [...elements].reduce((accum, subElement) => {
+      accum[subElement.dataset.element] = subElement;
+
+      return accum;
+    }, {});
+  }
+
+  setNewRange(from, to) {
+    this.range.from = from;
+    this.range.to = to;
+  }
+
+  formatHeading(reduce) {
+    return undefined;
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+    this.subElements = {};
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
 }
+
